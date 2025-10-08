29beb61aa2e80fe0bae44550e4a59ecc0ea611a4
Matteo Minin <matteominin@yahoo.it>
2025-10-08 11:27:58 +0200

    mocked API

---
+++ backend/src/main/java/com/lascaux/firminia/FirminiaApplication.java
@@ -0,0 +1,13 @@
+package com.lascaux.firminia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirminiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirminiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lascaux/firminia/FirminiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lascaux.firminia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirminiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
